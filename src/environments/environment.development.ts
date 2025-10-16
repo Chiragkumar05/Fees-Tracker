a261fba10030a57dce8b680090f1605a58885ad1
kokilapatelus <kokilapatelus@gmail.com>
2025-10-16 10:31:34 -0400

    small changes.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api',
+    API_URL: 'http://localhost:3000/api',
     appName: 'Fees Tracking App',
     version: '1.0.0'
 };
